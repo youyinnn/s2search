02d1e482bb386dc161798422314a2d2cfc6f3f27
DingLi23 <ding.li@mail.concordia.ca>
2022-01-17 21:20:25 -0500

    add s2search

--- s2search_example.py
+++ s2search_example.py
@@ -12,6 +12,46 @@ papers_example = [
         'authors': ['E Cambria', 'B White'],
         'year': 2014,
         'n_citations': 900,
+    },
+    {
+        'title': ' ',
+        'abstract': 'Natural language processing (NLP) is a theory-motivated range of computational techniques for the automatic analysis and representation of human language. NLP research has evolved from the era of punch cards and batch processing (in which the analysis of a sentence could take up to 7 minutes) to the era of Google and the likes of it (in which millions of webpages can be processed in less than a second). This review paper draws on recent developments in NLP research to look at the past, present, and future of NLP technology in a new light. Borrowing the paradigm of jumping curves from the field of business management and marketing prediction, this survey article reinterprets the evolution of NLP research as the intersection of three overlapping curves-namely Syntactics, Semantics, and Pragmatics Curveswhich will eventually lead NLP research to evolve into natural language understanding.',
+        'venue': 'IEEE Computational intelligence ',
+        'authors': ['E Cambria', 'B White'],
+        'year': 2014,
+        'n_citations': 900,
+    },
+    {
+        'title': 'Jumping NLP Curves: A Review of Natural Language Processing Research',
+        'abstract': ' ',
+        'venue': 'IEEE Computational intelligence ',
+        'authors': ['E Cambria', 'B White'],
+        'year': 2014,
+        'n_citations': 900,
+    },
+    {
+        'title': 'Jumping NLP Curves: A Review of Natural Language Processing Research',
+        'abstract': 'Natural language processing (NLP) is a theory-motivated range of computational techniques for the automatic analysis and representation of human language. NLP research has evolved from the era of punch cards and batch processing (in which the analysis of a sentence could take up to 7 minutes) to the era of Google and the likes of it (in which millions of webpages can be processed in less than a second). This review paper draws on recent developments in NLP research to look at the past, present, and future of NLP technology in a new light. Borrowing the paradigm of jumping curves from the field of business management and marketing prediction, this survey article reinterprets the evolution of NLP research as the intersection of three overlapping curves-namely Syntactics, Semantics, and Pragmatics Curveswhich will eventually lead NLP research to evolve into natural language understanding.',
+        'venue': ' ',
+        'authors': ['E Cambria', 'B White'],
+        'year': 2014,
+        'n_citations': 900,
+    },
+    {
+        'title': 'Jumping NLP Curves: A Review of Natural Language Processing Research',
+        'abstract': 'Natural language processing (NLP) is a theory-motivated range of computational techniques for the automatic analysis and representation of human language. NLP research has evolved from the era of punch cards and batch processing (in which the analysis of a sentence could take up to 7 minutes) to the era of Google and the likes of it (in which millions of webpages can be processed in less than a second). This review paper draws on recent developments in NLP research to look at the past, present, and future of NLP technology in a new light. Borrowing the paradigm of jumping curves from the field of business management and marketing prediction, this survey article reinterprets the evolution of NLP research as the intersection of three overlapping curves-namely Syntactics, Semantics, and Pragmatics Curveswhich will eventually lead NLP research to evolve into natural language understanding.',
+        'venue': 'IEEE Computational intelligence ',
+        'authors': [ ],
+        'year': 2014,
+        'n_citations': 900,
+    },
+    {
+        'title': 'Jumping NLP Curves: A Review of Natural Language Processing Research',
+        'abstract': 'Natural language processing (NLP) is a theory-motivated range of computational techniques for the automatic analysis and representation of human language. NLP research has evolved from the era of punch cards and batch processing (in which the analysis of a sentence could take up to 7 minutes) to the era of Google and the likes of it (in which millions of webpages can be processed in less than a second). This review paper draws on recent developments in NLP research to look at the past, present, and future of NLP technology in a new light. Borrowing the paradigm of jumping curves from the field of business management and marketing prediction, this survey article reinterprets the evolution of NLP research as the intersection of three overlapping curves-namely Syntactics, Semantics, and Pragmatics Curveswhich will eventually lead NLP research to evolve into natural language understanding.',
+        'venue': 'IEEE Computational intelligence ',
+        'authors': ['E Cambria', 'B White'],
+        'year': 2014,
+        'n_citations': 900,
     }
 ]
 
