dc190118913d90369bf082c0dad3b3e47d7b95c2
youyinnn <youyinnn@gmail.com>
2022-03-05 23:00:16 -0500

    ale analysis

--- plotting_nb_gen.py
+++ plotting_nb_gen.py
@@ -427,6 +427,7 @@ if not os.path.exists(pic_dir):
 sys.path.insert(1, '../../')
 import numpy as np, sys, os, pandas as pd
 from getting_data import read_conf
+pd.options.display.float_format = '{{:,.8f}}'.format
 
 sample_name = '{sample_name}'
 
@@ -439,7 +440,7 @@ f_list = [
     'n_citations'
     ]
 ale_rs = []
-ale_metric = pd.DataFrame(columns=['f1_name', 'f2_name', '2w_ale_range', 'mean'])
+ale_metric = pd.DataFrame(columns=['f1_name', 'f2_name', '2w_ale_range', 'mean', 'var', 'std'])
 
 def replace_quantile(feature_name, quantile):
     if feature_name == 'year' or feature_name == 'n_citations':
@@ -464,7 +465,9 @@ for i in range(len(f_list)):
             
             t = f'The mean of the 2-way ale - ({{f1_name}} * {{f2_name}}): {{np.mean(ale_result)}}'
             
-            ale_metric.loc[len(ale_metric.index)] = [f1_name, f2_name, np.max(ale_result) - np.min(ale_result), np.mean(ale_result)]
+            ale_metric.loc[len(ale_metric.index)] = \
+                [f1_name, f2_name, np.max(ale_result) - np.min(ale_result), np.mean(ale_result),\
+                    np.var(ale_result, ddof=1),np.std(ale_result, ddof=1)]
             
             ale = {{
                 'ale': ale_result,
@@ -488,16 +491,16 @@ for ale in ale_rs:
     fig, ax = plt.subplots(figsize=(14, 10), constrained_layout=True)
     cmap = plt.colormaps['BuGn']
     
-    # im = ax.pcolormesh(ale['f1_quantile'], ale['f2_quantile'], ale['ale'],  cmap=None, edgecolors='k', linewidths=0)
+    im = ax.pcolormesh(ale['f1_quantile'], ale['f2_quantile'], ale['ale'],  cmap=None, edgecolors='k', linewidths=0)
 
-    cf = ax.contourf(ale['f1_quantile'], ale['f2_quantile'], ale['ale'], levels=50, alpha=0.7)
+    # cf = ax.contourf(ale['f1_quantile'], ale['f2_quantile'], ale['ale'], levels=50, alpha=0.7)
     
     ax.set_xlabel(ale['f1_name'], fontsize=16, labelpad=10)
     ax.set_ylabel(ale['f2_name'], fontsize=16, labelpad=10)
     ax.xaxis.set_major_locator(MaxNLocator(integer=True))
     ax.yaxis.set_major_locator(MaxNLocator(integer=True))
     ax.set_title(ale['title'], fontsize=20, pad=10)
-    fig.colorbar(cf, ax=ax, shrink=0.6, pad=0.03)
+    fig.colorbar(im, ax=ax, shrink=0.6, pad=0.03)
     plt.savefig(os.path.join('.', 'plot', f"{sample_name}-2wale-{ale['f1_name']}-{ale['f2_name']}.png"), facecolor='white', transparent=False, bbox_inches='tight')
 '''
 
