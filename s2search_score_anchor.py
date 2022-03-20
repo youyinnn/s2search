3ac2064f64026092fc15b7ea1ec7ac3015431d11
youyinnn <youyinnn@gmail.com>
2022-03-20 21:47:55 +0000

    anchor metrics

--- s2search_score_anchor.py
+++ s2search_score_anchor.py
@@ -11,6 +11,7 @@ from anchor import anchor_tabular
 import pytz
 import datetime
 import logging
+import psutil
 
 utc_tz = pytz.timezone('America/Montreal')
 
@@ -156,7 +157,7 @@ def compute_and_save(output_exp_dir, output_data_sample_name, query, rg, data_ex
     logging.info(f'[{get_time_str()}] start computing anchor from index: {previous_work_idx} to {rg[1] - 1}')
     count = 0
     for i in range(previous_work_idx, rg[1]):
-        exp = explainer.explain_instance(paper_data[i], pred_fn, threshold=0.99, tau=0.5)
+        exp = explainer.explain_instance(paper_data[i], pred_fn, threshold=0.99, tau=0.8)
         
         previous_single_precision = 0
         for j in range(len(exp.names())):
@@ -171,7 +172,7 @@ def compute_and_save(output_exp_dir, output_data_sample_name, query, rg, data_ex
         
         if count % 10 == 0:
             ett = round(time.time() - sst, 6)
-            logging.info(f'[{get_time_str()}] ({i} / {rg[1] - rg[0] - 1}) {metrics_to_str(metrics)} within ({ett} total / {round(ett / count, 4)} average)')
+            logging.info(f'[{get_time_str()}] ({i} / {rg[1] - 1}) {metrics_to_str(metrics)} within ({ett} total / {round(ett / count, 4)} average)')
             save_pdp_to_npz('.', metrics_npz_file, 
                 title=metrics['title'],
                 abstract=metrics['abstract'],
@@ -186,35 +187,38 @@ def compute_and_save(output_exp_dir, output_data_sample_name, query, rg, data_ex
     
     logging.info(f'[{get_time_str()}] end anchor metrics witin {round(time.time() - st, 6)}')
 
-def get_anchor_metrics(exp_dir_path, sample_name, task_number):
-    des, sample_configs, sample_from_other_exp = read_conf(exp_dir_path)
+def get_anchor_metrics(exp_dir_path, sample_name, task_number, cpu_number):
+    p = psutil.Process()
+    worker = int(cpu_number)
+    print(f"Child #{worker}: {p}, affinity {p.cpu_affinity()}", flush=True)
+    time.sleep(1)
+    p.cpu_affinity([worker])
+    print(f"Child #{worker}: Set my affinity to {worker}, affinity now {p.cpu_affinity()}", flush=True)
 
-    tested_sample_list = []
+    des, sample_configs, sample_from_other_exp = read_conf(exp_dir_path)
     
     if sample_name in sample_from_other_exp.keys():
         other_exp_name, data_file_name = sample_from_other_exp.get(sample_name)
-        tested_sample_list.append({'exp_name': other_exp_name, 'data_sample_name': sample_name, 'data_source_name': data_file_name.replace('.data', '')})
+        tested_sample_config = {'exp_name': other_exp_name, 'data_sample_name': sample_name, 'data_source_name': data_file_name.replace('.data', '')}
     else:
-        tested_sample_list.append({'exp_name': exp_name, 'data_sample_name': sample_name, 'data_source_name': sample_name})
-
-    tested_sample_config = tested_sample_list[int(task_number) - 1]
-        
+        tested_sample_config = {'exp_name': exp_name, 'data_sample_name': sample_name, 'data_source_name': sample_name}
+    
     tested_sample_name = tested_sample_config['data_sample_name']
     tested_sample_data_source_name = tested_sample_config['data_source_name']
     tested_sample_from_exp = tested_sample_config['exp_name']
     
     task = sample_configs.get(tested_sample_name)
     if task != None:
-        for t in task:
-            query = t['query']
-            rg = t['range']
-            logging.basicConfig(filename=f"{tested_sample_name}_{rg}.log", encoding='utf-8', level=logging.INFO)
-            try:
-                compute_and_save(
-                    exp_dir_path, tested_sample_name, query, rg,
-                    tested_sample_from_exp, tested_sample_data_source_name)
-            except FileNotFoundError as e:
-                logging.error(e)
+        t = task[int(task_number) - 1]
+        query = t['query']
+        rg = t['range']
+        logging.basicConfig(filename=f"{tested_sample_name}_{rg}.log", encoding='utf-8', level=logging.INFO)
+        try:
+            compute_and_save(
+                exp_dir_path, tested_sample_name, query, rg,
+                tested_sample_from_exp, tested_sample_data_source_name)
+        except FileNotFoundError as e:
+            logging.error(e)
     else:
         logging.warning(f'**no config for tested sample {tested_sample_name}')
             
@@ -224,8 +228,9 @@ if __name__ == '__main__':
         exp_name = sys.argv[1]
         sample_name = sys.argv[2]
         task_number = sys.argv[3]
+        cpu_number = sys.argv[4]
         exp_dir_path = os.path.join(data_dir, exp_name)
         if os.path.isdir(exp_dir_path):
-            get_anchor_metrics(exp_dir_path, sample_name, task_number)
+            get_anchor_metrics(exp_dir_path, sample_name, task_number, cpu_number)
         else:
             print(f'**no exp dir {exp_dir_path}')
